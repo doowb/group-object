165c4e9c9f550d72032e0dcf62e6ee0764353dd0
Brian Woodward <brian.woodward@gmail.com>
2015-07-10 08:26:17 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * group-object <https://github.com/doowb/group-object>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
